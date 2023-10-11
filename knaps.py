7192cd60ba60174e3a91909b5519905376d518d7
Dhitaaprilia <112038650+Dhitaaprilia@users.noreply.github.com>
2023-10-12 02:35:32 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -127,13 +127,13 @@ with modeling:
 
         # Fitting Naive Bayes Classification to the Training set with linear kernel
         gaussian = GaussianNB()
-        gaussian = gaussian.fit(training, training_label)
+        gaussian = gaussian.fit(X_train, y_train)
 
         # Predicting the Test set results
-        y_pred = gaussian.predict(test)
+        y_pred = gaussian.predict(X_test)
     
         y_compare = np.vstack((test_label,y_pred)).T
-        gaussian.predict_proba(test)
+        gaussian.predict_proba(X_test)
         gaussian_akurasi = round(100 * accuracy_score(test_label, y_pred))
         # akurasi = 10
 
