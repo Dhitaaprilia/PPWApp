eb19d487ba8bb09c734c3a82303c11903dcb4bdd
Dhitaaprilia <112038650+Dhitaaprilia@users.noreply.github.com>
2023-10-12 02:44:56 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -50,7 +50,7 @@ with preporcessing :
     """)
     df = df.drop(columns=["Dokumen"])
     #Mendefinisikan Varible X dan Y
-    X = df.drop(columns=['Topik 1', 'Topik 2', 'Topik 3'])
+    X = df.drop(columns=['Topik 1'])
     y = df['Cluster'].values
     df
     X
@@ -63,7 +63,7 @@ with preporcessing :
     #scaler.transform(features)
     scaled = scaler.fit_transform(X)
     #features_names.remove('label')
-    scaled_features = pd.DataFrame(scaled, columns=['Topik 1', 'Topik 2', 'Topik 3'])
+    scaled_features = pd.DataFrame(scaled, columns=['Topik 1'])
 
     st.subheader('Hasil Normalisasi Data')
     st.write(scaled_features)
